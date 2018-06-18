36a28579962918a54d9836ed690bb8608e0695da
ViditGo <Vidit007vg@gmail.com>
2018-06-18 11:09:38 +0530

    initial

---
+++ src/lib/server.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+// env variables
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mern-starter';
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGODB_URI);
+
+app.use(bodyParser.json(),cors())
+
+app.all('*', (request, response) => {
+  console.log('Returning a 404 from the catch-all route');
+  return response.sendStatus(404);
+});
+
+// error middleware
+//app.use(require('./error-middleware'));
+
+
+const start = () => {
+  app.listen(PORT, () =>{
+    console.log(`Listening on port: ${PORT}`)
+  })
+}
+
+const stop = () => {
+  app.close(PORT, () => {
+    console.log(`Shut down on port: ${PORT}`)
+  })
+}
+
+module.exports = {
+  start,
+  stop
+}
